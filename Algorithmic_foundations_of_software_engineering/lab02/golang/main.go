f50e695130398355ff5099300ee0614ebf15121e
3xlerman <lermanworks@gmail.com>
2020-06-10 00:50:04 +0300

    afse lab02 finished

---
+++ Algorithmic_foundations_of_software_engineering/lab02/golang/main.go
@@ -0,0 +1,361 @@
+package main
+
+import (
+	"fmt"
+	"math"
+	"os"
+	"strings"
+)
+
+const (
+	// L = lines
+	L = 100
+
+	// A = assignments
+	A = 100
+
+	// P = powers
+	P = 100
+)
+
+var (
+	// number of lines and assignments
+	lineN, perN int
+
+	// maximum power of every assign in every stroke
+	maxP [L][A]int
+
+	// F factors array
+	F [L][A][P]float64
+
+	// D derivative factors array
+	D [L][A][P]float64
+
+	// array of values
+	values [A]float64
+
+	// array of minimum values
+	initValues [A]float64
+
+	// array of gradient values
+	grad [A]float64
+
+	// epsilon
+	epsilon float64
+
+	// G gradient of all assignments value
+	G float64
+
+	bound float64 = 10000000000000.01
+
+	isQ int
+
+	tMin float64 = 1.128
+)
+
+func main() {
+	input()
+	inputValues()
+	gradientDescent()
+	deleteValues()
+	conjugateGradients()
+}
+
+func gradientDescent() {
+	fmt.Println("\nРешение системы нелинейных уравнений методом градиентного спуска")
+	fmt.Print("----------------------------------------------------------------\n")
+	derivative()
+	// step of descent
+	step := 0.0001
+
+	fmt.Println("Метод градиентного спуска в процессе...")
+	iter := 0
+	for {
+		for j := 0; j < perN; j++ {
+			grad[j] = gradient(0, values[j], j)
+		}
+		G = calculateG(0, grad)
+		if G < epsilon {
+			break
+		}
+		for j := 0; j < perN; j++ {
+			values[j] = values[j] - step*grad[j]/float64(perN)
+		}
+		result, bound := builder(0, bound)
+		if result > bound {
+			fmt.Println("Градиент не сходится")
+			break
+		}
+		iter++
+	}
+	// output result and x values
+	fmt.Print("Ф(Х)= ", bound, "\n")
+	fmt.Print("Ответ:\n")
+	for j := 0; j < perN; j++ {
+		fmt.Println("x[", j, "]= ", values[j])
+	}
+	fmt.Println("Количество итераций:", iter)
+	fmt.Println("----------------------------------------------------------------\n")
+}
+
+func conjugateGradients() {
+	fmt.Println("\nРешение системы нелинейных уравнений методом сопряжённых градиентов")
+	fmt.Print("----------------------------------------------------------------\n")
+	isQ = 0
+	iter := 0
+	for j := 0; j < perN; j++ {
+		values[j] = initValues[j]
+	}
+	fmt.Println("Метод сопряжённых градиентов в процессе...")
+	for {
+		if iter == 0 {
+			for j := 0; j < perN; j++ {
+				grad[j] = gradient(0, values[j], j)
+			}
+
+			G = calculateG(0, grad)
+
+			if G < epsilon {
+				break
+			}
+
+			t := findStep(values, grad)
+
+			for j := 0; j < perN; j++ {
+				values[j] = values[j] - t*G
+			}
+
+			iter++
+
+		} else {
+			gradPrev := grad
+			for j := 0; j < perN; j++ {
+				grad[j] = gradient(0, values[j], j)
+			}
+			GPrev := G
+			G = calculateG(0, grad)
+
+			if G < epsilon {
+				break
+			}
+			var b float64
+			if isQ == 1 {
+				b = math.Pow(G, 2) / math.Pow(GPrev, 2)
+			} else {
+				z := 0.0
+				c := 0.0
+				for j := 0; j < perN; j++ {
+					z = z + grad[j]*(grad[j]-gradPrev[j])
+					c = c + grad[j]*grad[j]
+				}
+				b = z / c
+			}
+
+			d := G + b*GPrev
+
+			t := findStep(values, grad)
+
+			for j := 0; j < perN; j++ {
+				values[j] = values[j] - t*d
+			}
+
+			iter++
+		}
+
+	}
+	_, bound := builder(0, bound)
+	fmt.Print("Ф(Х)= ", bound, "\n")
+	fmt.Print("Ответ:\n")
+	for j := 0; j < perN; j++ {
+		fmt.Println("x[", j, "]= ", values[j])
+	}
+	fmt.Println("Количество итераций: ", iter)
+	fmt.Println("----------------------------------------------------------------\n")
+
+}
+
+func findStep(values [A]float64, grad [A]float64) float64 {
+	min := 1e+308
+	for j := 0; j < perN; j++ {
+		g := 0.0
+		for t := 0.001; t < 1000; t *= 1.1 {
+
+			g = math.Abs(2 * solve(0, values[j]-t*grad[j], F, 0, j, maxP[0][j]) * derivativeValue(0, values[j]-t*grad[j], F, maxP[0][j], 0, j))
+			if g < min {
+				tMin = t
+				break
+			}
+		}
+	}
+	return tMin
+}
+
+// detecting error type
+func errCheck(err error) bool {
+	if err != nil {
+		if strings.Contains(err.Error(), "expected integer") {
+			fmt.Println("Неверный тип данных")
+			return true
+		}
+		if strings.Contains(err.Error(), "value out of range") {
+			fmt.Println("Выход за границы диапазона")
+			return true
+		}
+		if strings.Contains(err.Error(), "invalid syntax") {
+			fmt.Println("Неверный тип данных")
+			return true
+		}
+	}
+	return false
+}
+
+// const numbers input
+func inputNumbers() {
+	var temp float64
+
+	fmt.Print("Количество строк в системе: ")
+	_, err := fmt.Scan(&temp)
+	if errCheck(err) {
+		fmt.Println("Ошибка в вводе количества строк")
+		os.Exit(1)
+	}
+	lineN = int(temp)
+
+	fmt.Print("Количество переменных в системе: ")
+	_, err = fmt.Scan(&temp)
+	if errCheck(err) {
+		fmt.Println("Ошибка в вводе количества переменных")
+		os.Exit(1)
+	}
+	perN = int(temp)
+
+	if lineN < perN {
+		fmt.Println("Система неразрешима")
+		os.Exit(1)
+	}
+
+}
+
+// system factors input
+func inputFactors() {
+	var temp float64
+	for i := 0; i < lineN; i++ {
+		fmt.Print("### Строка ", i+1, ": ###\n")
+		for j := 0; j < perN; j++ {
+
+			fmt.Print("Переменная x[", j, "]:\n")
+			fmt.Print("Высшая степень x[", j, "]: ")
+
+			_, err := fmt.Scan(&temp)
+			if errCheck(err) {
+				fmt.Println("Ошибка в вводе высшей степени переменной x[", j, "]")
+				os.Exit(1)
+			}
+			maxP[i][j] = int(temp)
+
+			for k := maxP[i][j]; k >= 0; k-- {
+
+				fmt.Print("Введите коэффициент при степени ", k, ": ")
+
+				_, err = fmt.Scan(&F[i][j][k])
+				if errCheck(err) {
+					fmt.Println("Ошибка в вводе коэффициента ", k, " степени переменной x[", j, "]")
+					os.Exit(1)
+				}
+
+			}
+		}
+	}
+}
+
+// init valuse input
+func inputValues() {
+	fmt.Print("### Ввод начальных значений ###\n")
+	for j := 0; j < perN; j++ {
+		fmt.Print("Начальное значение переменной x[", j, "]= ")
+		_, err := fmt.Scan(&values[j])
+		initValues[j] = values[j]
+		if errCheck(err) {
+			fmt.Println("Ошибка в вводе начальных значений")
+			os.Exit(1)
+		}
+	}
+	// user bound input
+	fmt.Print("### Ввод порога сходимости (epsilon) ###\n")
+	_, err := fmt.Scan(&epsilon)
+	if errCheck(err) {
+		fmt.Println("Ошибка в вводе порога")
+		os.Exit(1)
+	}
+}
+
+// group input functions in big one
+func input() {
+	inputNumbers()
+	inputFactors()
+}
+
+// create derivative factors of every stroke
+func derivative() {
+	for i := 0; i < lineN; i++ {
+		for j := 0; j < perN; j++ {
+			for k := 0; k < maxP[i][j]; k++ {
+				D[i][j][k] = F[i][j][k+1] * float64(k+1)
+			}
+		}
+	}
+}
+
+// create derivative factors of every stroke
+func derivativeValue(sum float64, x float64, mas [L][A][P]float64, power int, i int, j int) float64 {
+	for k := power; k > 0; k-- {
+		sum = sum + float64(k)*mas[i][j][k]*math.Pow(x, float64(power-1))
+	}
+	return sum
+}
+
+// solving a polinom, derivative or not
+func solve(res float64, x float64, mas [L][A][P]float64, i int, j int, power int) float64 {
+	for k := power; k >= 0; k-- {
+		res = res*x + mas[i][j][k]
+	}
+	return res
+}
+
+// calculating gradient for every assignment
+func gradient(sum float64, x float64, j int) float64 {
+	for i := 0; i < lineN; i++ {
+		sum = sum + 2*solve(0, x, F, i, j, maxP[i][j])*solve(0, x, D, i, j, maxP[i][j]-1)
+	}
+	return sum
+}
+
+// final output Ф(x)
+func builder(sum float64, bound float64) (result float64, resultMin float64) {
+	for j := 0; j < perN; j++ {
+		for i := 0; i < lineN; i++ {
+			sum = sum + math.Pow(solve(0, values[j], F, i, j, maxP[i][j]), 2)
+		}
+	}
+	if sum < bound {
+		bound = sum
+	}
+	return sum, bound
+}
+
+func calculateG(sum float64, grad [A]float64) float64 {
+	for j := 0; j < perN; j++ {
+		sum = sum + grad[j]*grad[j]
+	}
+	return math.Sqrt(sum)
+}
+
+func deleteValues() {
+
+	for j := 0; j < perN; j++ {
+		values[j] = 0
+		grad[j] = 0
+	}
+
+	bound = 10000000000000.01
+}
